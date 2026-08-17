b71e9c036a8c646cd7ea6fa439fe4baed12e06b6
rodolfo <rodolfotiago@gmail.com>
2026-08-17 10:08:34 -0300

    testing save user mock

--- src/models/User.js
+++ src/models/User.js
@@ -36,16 +36,11 @@ const userSchema = new mongoose.Schema(
 );
 
 // Hash automático da senha antes de salvar
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
 
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 // Método para validar senha
